e418566affad4829235f4f7ae7af0af02a6970a6
Kevin Lin <kevin.lin.p@gmail.com>
2021-02-21 16:44:18 -0500

    styling Artist

--- imports/ui/Artist.svelte
+++ imports/ui/Artist.svelte
@@ -1,67 +1,55 @@
 <script>
-  export let key;
-  export let artist;
   import { MusicResults } from '../api/musicResults.js'
   import { useTracker, useSession } from 'meteor/rdb:svelte-meteor-data';
   import { Session } from 'meteor/session'
 
+  import ArtistMusic from './ArtistMusic.svelte'
+
+  export let key
+  export let artist
+
   const currentArtistId = useSession('currentArtistId')
 
   $: isCurrentArtist = ($currentArtistId == key)
-  $: musicResults = useTracker(() => MusicResults.find({artistId: artist._id}, {sort: {postedAt: -1}, limit: 5}).fetch());
 
   function handleArtistNameClick() {
-    Session.set('currentArtistId', key)
-    if (!artist.mixcloudSearchedAt) {
-      Meteor.call("artists.search", artist._id);
+    if (isCurrentArtist) {
+      Session.set('currentArtistId', '')
+    } else {
+      Session.set('currentArtistId', key)
+      if (!artist.mixcloudSearchedAt) {
+        Meteor.call("artists.search", artist._id);
+      }
     }
   }
-
-  function handleArtistNameClearClick() {
-    Session.set('currentArtistId', '')
-  }
-
-    /* <div class="ms-auto"> */
-    /*   <button class="btn btn-dark btn-sm" on:click={handleSearch}>search</button> */
-    /* </div> */
 </script>
 
 <style>
-  a {
+  .artist-name {
     text-decoration: none;
+  }
+
+  .artist-name.active {
+    color: inherit;
   }
 </style>
 
 <div>
-  <div class="d-flex align-items-end mb-4">
-  {#if isCurrentArtist}
-    <div class="display-3" style="line-height: 1.0;" on:click={handleArtistNameClearClick}>
+  <div class="d-flex align-items-end mb-3">
+    <a
+      on:click={handleArtistNameClick}
+      class="artist-name display-3"
+      style="width: 100%; line-height: 1.0;"
+      class:active={isCurrentArtist}
+      href="javascript:void(0);"
+    >
       { artist.name }
-    </div>
-  {:else}
-    <a on:click={handleArtistNameClick} href="javascript:void(0);">
-      <div class="display-3" style="line-height: 1.0;">
-        { artist.name }
-      </div>
     </a>
-  {/if}
   </div>
 
-
   {#if isCurrentArtist}
-
-  {#if !artist.mixcloudSearchedAt}
-    <h1 class="my-5">searching Mixcloud...</h1>
-  {/if}
-
-  {#each $musicResults as musicResult}
-    <div>
-      <iframe width="100%" height="60" src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&feed={musicResult.key}" frameborder="0" ></iframe>
-      { musicResult.postedAt }
-
+    <div class="ms-4">
+      <ArtistMusic artist={artist} />
     </div>
-  {/each}
   {/if}
-
 </div>
-
---
+++ imports/ui/ArtistMusic.svelte
@@ -0,0 +1,27 @@
+<script>
+  import { MusicResults } from '../api/musicResults.js'
+  import { useTracker, useSession } from 'meteor/rdb:svelte-meteor-data';
+
+  export let artist;
+
+  $: musicResults = useTracker(() => MusicResults.find({artistId: artist._id}, {sort: {postedAt: -1}, limit: 5}).fetch());
+
+  function formatDate(date) {
+    return date.toLocaleDateString(undefined, {month: 'short', year: 'numeric'})
+  }
+</script>
+
+<div>
+  {#if !artist.mixcloudSearchedAt}
+    <h1 class="display-1 my-5">searching Mixcloud...</h1>
+  {/if}
+
+  {#each $musicResults as musicResult}
+    <div class="d-flex flex-column mb-3">
+      <div class="align-self-end me-2 text-muted" style="line-height: 1.0;">{ formatDate(musicResult.postedAt) }</div>
+
+      <iframe width="100%" height="60" src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&feed={musicResult.key}" frameborder="0"></iframe>
+    </div>
+  {/each}
+</div>
+
