73ac82b645971d73d1005d93decd6768271458dd
Kevin Lin <kevin.lin.p@gmail.com>
2021-02-21 15:44:58 -0500

    display FestivalArtists

--- imports/ui/Festival.svelte
+++ imports/ui/Festival.svelte
@@ -3,6 +3,7 @@
   import Artist from './Artist.svelte';
   import CreateFestivalArtist from './CreateFestivalArtist.svelte';
   import { Artists } from '../api/artists.js'
+  import { FestivalArtists } from '../api/festivalArtists.js'
   import { Meteor } from "meteor/meteor";
   import { onMount } from 'svelte';
 
@@ -16,7 +17,14 @@
   });
 
   $: festival = useTracker(() => Festivals.findOne({slug: 'movement-2019'}).fetch());
-  $: artists = useTracker(() => Artists.find({}, {sort: {name: 1}}).fetch());
+  $: artists = useTracker(() => {
+    if (!festival) { return [] }
+
+    const festivalArtists = FestivalArtists.find({festivalId: festival.id}).fetch()
+    const artistIds = festivalArtists.map((a) => a.artistId)
+
+    return Artists.find({_id: {$in: artistIds}}, {sort: {name: 1}}).fetch()
+  });
 </script>
 
 
